751e166a2987bbb96d84275274df49148fb09442
Karinna Iniguez <karinnainiguez@gmail.com>
2018-06-19 10:25:13 -0700

    Add create method to movies controller and route for it

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,8 +17,34 @@ class MoviesController < ApplicationController
       json: @movie.as_json(
         only: [:title, :overview, :release_date, :inventory],
         methods: [:available_inventory]
+      )
+    )
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.inventory = rand(0..10)
+
+    if @movie.save
+      render(
+        status: :ok,
+        json: @movie.as_json(
+          only: [:title, :overview, :release_date, :inventory],
+          methods: [:available_inventory]
         )
       )
+
+    else
+      render(
+        status: :bad_request,
+        json: {
+          errors: {
+            movie: ["Movie #{@movie.title} could not be added to our database."]
+          }
+        }
+      )
+
+    end
   end
 
   private
@@ -28,5 +54,10 @@ class MoviesController < ApplicationController
     unless @movie
       render status: :not_found, json: { errors: { title: ["No movie with title #{params["title"]}"] } }
     end
+  end
+
+  def movie_params
+    return params.permit(:title, :overview, :release_date, :image_url, :external_id)
+
   end
 end
