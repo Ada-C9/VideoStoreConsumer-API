8b29c4c189bd83f006e0d36e57a05dca983eca28
Karinna Iniguez <karinnainiguez@gmail.com>
2018-06-20 15:53:22 -0700

    Add rental and movie tests and pass with validations

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -16,7 +16,7 @@ class MovieTest < ActiveSupport::TestCase
 
   describe "Constructor" do
     it "Can be created" do
-      Movie.create!(movie_data)
+      Movie.create!(title: "Dumbo", external_id: 900)
     end
 
     it "Has rentals" do
@@ -47,7 +47,7 @@ class MovieTest < ActiveSupport::TestCase
         customer: customers(:one),
         movie: movie,
         due_date: Date.today + 7,
-        returned: false
+        returned: nil
       )
 
       movie.reload
@@ -64,7 +64,7 @@ class MovieTest < ActiveSupport::TestCase
         customer: customers(:one),
         movie: movie,
         due_date: Date.today + 7,
-        returned: false
+        returned: nil
       )
 
       movie.reload
@@ -76,6 +76,42 @@ class MovieTest < ActiveSupport::TestCase
       movie.reload
       after_ai = movie.available_inventory
       after_ai.must_equal before_ai + 1
+    end
+  end
+
+  describe "validations" do
+    it "cannot be created without a title" do
+      old_count = Movie.count
+      movie = Movie.new
+      movie.valid?.must_equal false
+
+      movie.save
+      movie.errors.messages.must_include :title
+
+      Movie.count.must_equal old_count
+    end
+
+    it "can successfully be created with title" do
+      title = "some title"
+      old_count = Movie.count
+      movie = Movie.new(title: "300", external_id: 867)
+      movie.valid?.must_equal true
+
+      movie.save
+
+      Movie.count.must_equal old_count + 1
+    end
+
+    it "cannot be created with duplicate external id" do
+      reused_id = 123
+      Movie.create!(title: "something", external_id: reused_id)
+      old_count = Movie.count
+
+      new_movie = Movie.new(title: "whatever", external_id: reused_id)
+      new_movie.valid?.must_equal false
+      new_movie.save
+      new_movie.errors.messages.must_include :external_id
+      Movie.count.must_equal old_count
     end
   end
 end
