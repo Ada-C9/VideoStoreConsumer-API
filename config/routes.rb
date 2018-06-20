90de80007d8c1ba99d7bed715e26cebb8f6d7c43
LetiTran <leticiastran@gmail.com>
2018-06-20 14:26:05 -0700

    Adds CORS Fix for 'unauthorized'problem given on Chrome for POST requests since it has the same origin. Also modified routes for easier access with our react app.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :movies, only: [:index, :show], param: :title
 
-  post "/rentals/:title/check-out", to: "rentals#check_out", as: "check_out"
+  post "/rentals/check-out", to: "rentals#check_out", as: "check_out"
   post "/rentals/:title/return", to: "rentals#check_in", as: "check_in"
   get "/rentals/overdue", to: "rentals#overdue", as: "overdue"
 
