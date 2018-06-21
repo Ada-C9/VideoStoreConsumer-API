546ae8a4596b41a761c53991a2745ff8d65e8596
Abiaina <abinnet.a@gmail.com>
2018-06-20 17:03:51 -0700

    added post add_movie functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,8 +17,32 @@ class MoviesController < ApplicationController
       json: @movie.as_json(
         only: [:title, :overview, :release_date, :inventory],
         methods: [:available_inventory]
-        )
       )
+    )
+  end
+
+  def add_movie
+    #pass in new_movie from params...somehow..its an object
+    @new_movie = params[:movie]
+
+    if Movie.find_by(external_id: @new_movie.external_id)
+      render(
+        status: :bad_request, json: { errors: "already in movie library" }
+      )
+    else
+      @new_movie = MovieWrapper.construct_movie(@new_movie)
+      if @new_movie.save
+        render(
+          status: :ok,
+          json: @new_movie.as_json(
+            # also can return external_id and then have the front end do an api call again
+            only: [:title, :overview]
+          )
+        )
+      else
+        render status: :bad_request, json: { errors: rental.errors.messages }
+      end
+    end
   end
 
   private
