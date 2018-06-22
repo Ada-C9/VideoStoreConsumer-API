873f7a4b3bde235cb7e4525b665a64bd2edd339f
LetiTran <leticiastran@gmail.com>
2018-06-21 22:55:02 -0700

    Fix checked-out movie count to display correclty in front-end

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,7 @@ class Customer < ApplicationRecord
   has_many :movies, through: :rentals
 
   def movies_checked_out_count
-    self.rentals.where(returned: false).length
+    # self.rentals.where(returned: false).length
+    self.rentals.count
   end
 end
